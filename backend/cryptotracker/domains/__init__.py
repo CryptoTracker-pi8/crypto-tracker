e7a35d4abcd187da280b600dd7697916ff497bf1
deadshumz <shumzz@bk.ru>
2025-11-08 15:52:44 +0300

    feat: add favorites API endpoints (POST, GET, DELETE)

--- backend/cryptotracker/domains/__init__.py
+++ backend/cryptotracker/domains/__init__.py
@@ -1,5 +1,7 @@
+from cryptotracker.domains.favorites.router import router as favorites_router
 from cryptotracker.domains.healthcheck.router import router as healthcheck_router
 
 router_list = [
-    healthcheck_router
+    healthcheck_router,
+    favorites_router,
 ]
